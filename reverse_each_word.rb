a7074ee5ad282dfaf494ce4d7f5d8b2270d99914
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-26 16:14:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,6 +9,8 @@ end
 
 def reverse_each-word(string)
 new_array = string.split(" ")
-new_array.collect do |word| word.reverse 
+reverse_array=[]
+new_array.collect do |word| reverse_array << word.reverse 
 end 
+reverse_array.join(" ")
 end 
