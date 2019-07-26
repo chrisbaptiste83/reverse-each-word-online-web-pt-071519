e364f62d1073e8f3b58399ce1aa93dfd9d4261da
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-26 16:05:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,3 @@ end
 reverse_array.join("")
 end
 
-def reverse_each_word(string)
-  new_array = string.split(" ") 
-reverse_array = []
-  array.collect do|word| 
-    reverse_array << word.reverse 
-  end
-  reverse_array.join(" ")
-end
